ccf1f1d3883fb222f75124f2f3e7c56d58eff2e7
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2018-02-19 18:42:15 +0000

    working local

--- eisitirio/system.py
+++ eisitirio/system.py
@@ -18,9 +18,15 @@ import jinja2
 import app
 APP = app.APP
 
-APP.config.from_pyfile('config/default.py')
-APP.config.from_pyfile('config/ticket_types.py')
-APP.config.from_pyfile('config/postage.py')
+import dbconfig
+if dbconfig.dbhost=='localhost':
+    APP.config.from_pyfile('config/default.py')
+    APP.config.from_pyfile('config/ticket_types.py')
+    APP.config.from_pyfile('config/postage.py')
+else:
+    APP.config.from_pyfile('config/default.py')
+    APP.config.from_pyfile('config/ticket_types.py')
+    APP.config.from_pyfile('config/postage.py')
 #
 # from flask_sqlalchemy import SQLAlchemy
 
