3c257e20d9af035289df0d38debf231c86c2ddb0
qvandenberg <quincy.vandenberg@physics.ox.ac.uk>
2018-03-08 09:34:16 +0000

    realex info

--- eisitirio/config/payment.py
+++ eisitirio/config/payment.py
@@ -2,5 +2,5 @@ ENABLE_ROUNDUP_DONATION = False
 REALEX_MERCHANT_ID='ouss'
 REALEX_SECRET='xZq1xMsheb'
 
-REALEX_ENDPOINT_URL='asjdkhaskj'
-REALEX_RESPONSE_URL='asdjkhadkj'
+REALEX_ENDPOINT_URL="https://hpp.realexpayments.com/pay"
+REALEX_RESPONSE_URL='http://0.0.0.0:5001/purchase/payment-processed'
