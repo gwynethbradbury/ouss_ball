7fcef17d52fb3ce556a39805b7a1828092f15f76
Gwyneth <gwyneth.bradbury@gmail.com>
2019-03-18 23:37:04 +0000

    -

--- eisitirio/config/production.py
+++ eisitirio/config/production.py
@@ -29,7 +29,7 @@ EWAY_API_PASSCODE = base64.b64encode(
 
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 587
-SMTP_SSL = True
+SMTP_SSL = False
 SMTP_STARTTLS = True
 SMTP_LOGIN = True
 SMTP_USER = 'oxfordsalsaball2019@gmail.com'
