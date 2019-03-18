bb64b653a813c783e30726457e685ed5dc565b54
Gwyneth <gwyneth.bradbury@gmail.com>
2019-03-18 22:41:06 +0000

    -

--- eisitirio/config/ticket_types.py
+++ eisitirio/config/ticket_types.py
@@ -8,24 +8,27 @@ from eisitirio.helpers import ticket_type
 
 # The default ticket type must count towards the guest limit, as it is the
 # ticket type assigned to people on the waiting list.
-DEFAULT_TICKET_TYPE = ticket_type.TicketType("Standard Ball Only", "standard_ball", 2300, 10,
+DEFAULT_TICKET_TYPE = ticket_type.TicketType("Standard Ball Only", "standard_ball", 2500, 10,
                                              -1, True,
                                              False)#ticket_logic.can_buy_standard)
 
 TICKET_TYPES = [
-    ticket_type.TicketType("Standard Ball + Workshops (Early Bird)", "early_standard_ball", 2300, 1, 5,
+    ticket_type.TicketType("OUSS Member Ball + Workshops (Early Bird)", "early_member_both", 2000, 1, 5,
                            True, True),
-    ticket_type.TicketType("Standard Ball + Workshops", "standard_both", 2800, 1, 70,
+    ticket_type.TicketType("OUSS Member Ball + Workshops", "member_both", 2500, 1, 70,
                            True, True),
+    ticket_type.TicketType("OUSS Member Ball Only", "member_ball", 2000, 1, -1,
+                           True, False),
+
+    ticket_type.TicketType("Standard Ball + Workshops (Early Bird)", "early_standard_ball", 2500, 1, 5,
+                           True, True),
+    ticket_type.TicketType("Standard Ball + Workshops", "standard_both", 3000, 1, 70,
+                           True, True),
+
+
     # ticket_type.TicketType("Standard Ball Only", "early_standard_both", 2300, 1, 5,
     #                        True, False),
 
-    ticket_type.TicketType("OUSS Member Ball Only", "member_ball", 2000, 1, -1,
-                           True, False),
-    ticket_type.TicketType("OUSS Member Ball + Workshops", "member_both", 2500, 1, 70,
-                           True, True),
-    ticket_type.TicketType("OUSS Member Ball + Workshops (Early Bird)", "early_member_both", 2000, 1, 5,
-                           True, True),
 
 
     # ticket_type.TicketType("OUSS Comittee", "comittee", 0, 1, -1,
