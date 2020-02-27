0429766c462b91078e07d3f1f89fe5bb47f46b19
Gwyneth <gwyneth.bradbury@gmail.com>
2020-02-27 14:32:59 +0000

    updated app passwrd

--- eisitirio/config/production.py
+++ eisitirio/config/production.py
@@ -33,7 +33,7 @@ SMTP_SSL = True
 SMTP_STARTTLS = False
 SMTP_LOGIN = True
 SMTP_USER = 'oxfordsalsaball2020@gmail.com'
-SMTP_PASSWORD = 'blagoevgrad'
+SMTP_PASSWORD = 'ecdrgvozwxmtqkbv'
 
 AWS_ACCESS_KEY_ID = 'AWS Access Key ID'
 AWS_SECRET_ACCESS_KEY = 'AWS Secret Access Key'
