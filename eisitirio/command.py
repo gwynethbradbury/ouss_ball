6dc62cdfa5a90f6790f01900f81dd4c4f1b34c5c
Gwyneth <gwyneth.bradbury@gmail.com>
2023-02-28 00:59:53 +0000

    front end ready

--- eisitirio/command.py
+++ eisitirio/command.py
@@ -25,7 +25,10 @@ from eisitirio.scripts import send_full_list
 from eisitirio.scripts import create_member_accounts
 
 EISITIRIO_DIR = os.path.realpath(__file__).replace('command.py',
-                                                   '')
+                                                   'eisitirio')
+#LOCAL:
+# EISITIRIO_DIR = os.path.realpath(__file__).replace('command.py',
+#                                                    '')
 
 def get_app(config):
     """Load the appropriate config into the app."""
