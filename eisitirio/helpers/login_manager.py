b290257ec61c2f4ca33a4b67bdb435267f1c7062
Gwyneth <gwyneth.bradbury@gmail.com>
2023-03-06 01:06:02 +0000

    sandbox paypal

--- eisitirio/helpers/login_manager.py
+++ eisitirio/helpers/login_manager.py
@@ -24,7 +24,13 @@ def load_user(user_id):
     if flask.current_app.config['MAINTENANCE_MODE']:
         return LOGIN_MANAGER.anonymous_user
     else:
-        return models.User.query.get_or_404(user_id)
+        try:
+            return models.User.query.get_or_404(user_id)
+        except Exception as e:
+            print(e)
+            login.logout_user()
+            return flask.redirect(flask.url_for('front.home'))
+
 
 LOGIN_MANAGER.login_view = 'front.home'
 LOGIN_MANAGER.session_protection = 'basic'
